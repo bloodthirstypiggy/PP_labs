46d4f1493e708d4148a9e2e1e5989158a0e24a74
bloodthirstypiggy <bloodthirstypiggy@gmail.com>
2022-09-11 21:47:39 +0300

    work?

--- MP_Lab1/lab1.c
+++ MP_Lab1/lab1.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
     const int random_seed = 920215; ///< RNG seed
     int column = 10;
 
-    int* array = 0;                 ///< The array we need to find the max in
+    int** array = 0;                 ///< The array we need to find the max in
     int  max   = -1;                ///< The maximal element
     int t = 0;                   ///  Used time
     int t_end=0;
@@ -21,22 +21,34 @@ int main(int argc, char** argv)
     printf("OpenMP: %d;\n======\n", _OPENMP);
 
     /* Generate the random array */
-    array = (int*)malloc(count * sizeof(int)); //line * column
-    for(int i=0; i<count; i++) { array[i] = rand(); }
-
+    array = (int**)malloc(column * sizeof(int*)); //line * column
+    for(int i=0; i<column;i++)
+    {
+        array[i] = (int*)malloc(count * sizeof(int));
+    }
+    for(int i=0; i<column; i++)
+    {
+        for(int j=0; j<count;j++)
+        {
+            array[i][j] = rand();
+        }
+    }
 
     for(int j=1; j<threads + 1; j++)
     {
     t = clock();
 
-//        for(int k = 0; k<column; k++){
     /* Find the maximal element */
-    #pragma omp parallel num_threads(j) shared(array, count) reduction(max: max) default(none)
+    #pragma omp parallel num_threads(j) shared(array, count, column) reduction(max: max) default(none)
     {
         #pragma omp for
-        for(int i=0; i<count; i++)
+        for(int i=0; i<column; i++)
         {
-            if(array[i] > max) { max = array[i]; };
+            for(int j=0; j<count; j++){
+            if(array[i][j] > max) {
+                max = array[i][j]; 
+            }
+            }
         }
         printf("-- My lmax is: %d;\n", max);
     
@@ -47,7 +59,6 @@ int main(int argc, char** argv)
     t = clock() - t;
     time_taken = ((double)t)/CLOCKS_PER_SEC;
     printf("time spent is %f \n", time_taken);
-//}
 }
 return 0;
 }
