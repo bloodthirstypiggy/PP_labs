476ee266c5447a71814647e6d5a14c9d85adc9ae
terrified piggy <64132550+bloodthirstypiggy@users.noreply.github.com>
2022-09-18 20:05:35 +0300

    Delete script.py

--- MP_Lab1/script.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-fd = open("test.txt", "r")
-file = fd.read()
-file = file.replace(".000000", "")
-file = file.split()
-osi = range(0,1)
-plt.plot((1,2,3,4,5,6,7,8), osi)
-plt.show()
